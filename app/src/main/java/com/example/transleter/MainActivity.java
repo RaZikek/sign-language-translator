300b22acd9a2735fd5ca5809a04474be614c6f37
RaZikek <dima_antonov_2005@list.ru>
2021-04-15 19:51:23 +0300

    1

--- app/src/main/java/com/example/transleter/MainActivity.java
+++ app/src/main/java/com/example/transleter/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     VideoView videoView;
     Button button;
     TextView textView;
+    EditText search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +25,13 @@ public class MainActivity extends AppCompatActivity {
         videoView = findViewById(R.id.videoView);
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView2);
+        search = findViewById(R.id.search);
         SharedPreferences pref = getSharedPreferences("myPref", MODE_PRIVATE);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                   videoView.setVideoURI(Uri.parse(pref.getString("car", null)));
+                   videoView.setVideoURI(Uri.parse(pref.getString(search.getText().toString(), null)));
                    videoView.start();
-                 //  textView.setText(pref.getAll().toString());
             }
         });
 
--- app/src/main/java/com/example/transleter/StartActivity.java
+++ app/src/main/java/com/example/transleter/StartActivity.java
@@ -21,7 +21,14 @@ public class StartActivity extends AppCompatActivity {
             startActivity(i);
         }else {
             editor.putBoolean("first", true);
-            editor.putString("car", "https://media.spreadthesign.com/video/mp4/12/345841.mp4");
+            editor.putString("машина", "https://media.spreadthesign.com/video/mp4/12/345841.mp4");
+            editor.putString("hell", "https://media.spreadthesign.com/video/mp4/12/349210.mp4");
+            editor.putString("address", "https://media.spreadthesign.com/video/mp4/12/4380.mp4");
+            editor.putString("asia", "https://media.spreadthesign.com/video/mp4/12/351481.mp4");
+            editor.putString("ameriсa", "https://media.spreadthesign.com/video/mp4/12/349176.mp4");
+            editor.putString("england", "https://media.spreadthesign.com/video/mp4/12/16122.mp4");
+            editor.putString("nice smell", "https://media.spreadthesign.com/video/mp4/12/349232.mp4");
+            editor.putString("africa", "https://media.spreadthesign.com/video/mp4/12/12723.mp4");
             editor.apply();
             startActivity(i);
         }
