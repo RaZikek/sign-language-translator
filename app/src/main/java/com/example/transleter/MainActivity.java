6968047b1efafd5d6acf9ebe15f2de4f46f43d46
Razi <dima_antonov_2005@list.ru>
2021-06-04 00:22:50 +0300

    1

--- app/src/main/java/com/example/transleter/MainActivity.java
+++ app/src/main/java/com/example/transleter/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -18,6 +22,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     VideoView videoView;
@@ -28,10 +33,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Закрепление экрана
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         videoView = findViewById(R.id.videoView);
         button = findViewById(R.id.send);
         search = findViewById(R.id.search);
         SharedPreferences pref = getSharedPreferences("myPref", MODE_PRIVATE);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,12 +54,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // Вывод видео
     public void changeUrl(String url) {
         videoView.setVideoURI(Uri.parse(url));
         videoView.start();
     }
 
-    class Pref extends AsyncTask<Void, String,String> {
+    // AsyncTask
+    class Pref extends AsyncTask<Void, String, String> {
 
 
         @Override
@@ -73,17 +85,16 @@ public class MainActivity extends AppCompatActivity {
             url = url + search.getText().toString();
             Elements videoE = doc.select("video");
             if (videoE != null) {
-                String videoSeearchUrl = videoE.attr("src");
-                return videoSeearchUrl;
+                String videoSearchUrl = videoE.attr("src");
+                return videoSearchUrl;
             }
             return null;
         }
 
         @Override
         protected void onPostExecute(String s) {
-            if (s != null) {
+            if (s == null) {
                 Toast.makeText(MainActivity.this, "Not found", Toast.LENGTH_SHORT).show();
-                return;
             }
             changeUrl(s);
         }
