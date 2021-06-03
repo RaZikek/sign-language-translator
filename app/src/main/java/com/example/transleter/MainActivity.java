0cc10e9dd97b468f8b58dc5af56caa7fce724b9c
RaZikek <dima_antonov_2005@list.ru>
2021-06-03 21:41:55 +0300

    1

--- app/src/main/java/com/example/transleter/MainActivity.java
+++ app/src/main/java/com/example/transleter/MainActivity.java
@@ -2,20 +2,26 @@ package com.example.transleter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     VideoView videoView;
     Button button;
-    TextView textView;
     EditText search;
 
     @Override
@@ -23,21 +29,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         videoView = findViewById(R.id.videoView);
-        button = findViewById(R.id.button);
+        button = findViewById(R.id.send);
         search = findViewById(R.id.search);
         SharedPreferences pref = getSharedPreferences("myPref", MODE_PRIVATE);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (search.getText().toString() != null) {
-                    videoView.setVideoURI(Uri.parse(pref.getString(search.getText().toString().toLowerCase(), null)));
+                if (!search.getText().toString().isEmpty()) {
+                    Pref pref1 = new Pref();
+                    pref1.execute();
+                    videoView.setVideoURI(Uri.parse(pref.getString("n", "https://media.spreadthesign.com/video/mp4/12/4136.mp4")));
                     videoView.start();
-                }else
-                    Toast.makeText(MainActivity.this,"Такого слова пока что не добавили. Извините",Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(MainActivity.this, "Введите слово", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void changeUrl(String url) {
+        videoView.setVideoURI(Uri.parse(url));
+        videoView.start();
+    }
+
+    class Pref extends AsyncTask<Void, String,String> {
 
 
+        @Override
+        protected String doInBackground(Void... voids) {
+            SharedPreferences pref = getSharedPreferences("myPref", MODE_PRIVATE);
+            SharedPreferences.Editor editor = pref.edit();
+            Document doc = null;
+
+            String url = "https://www.spreadthesign.com/ru.ru/search/?q=";
+
+            try {
+                doc = Jsoup.connect(url + search
+                        .getText()
+                        .toString())
+                        .userAgent("Chrome/4.0.249.0 Safari/532.5")
+                        .referrer("http://www.google.com")
+                        .get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            url = url + search.getText().toString();
+            Elements videoE = doc.select("video");
+            if (videoE != null) {
+                String videoSeearchUrl = videoE.attr("src");
+                return videoSeearchUrl;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null) {
+                Toast.makeText(MainActivity.this, "Not found", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            changeUrl(s);
+        }
     }
 
 
